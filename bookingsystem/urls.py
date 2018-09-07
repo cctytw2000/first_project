ec7d10e6fa2bfdc1612a572432e9df870a65f25b
kuanlinyeh <cctytw2000@gmail.com>
2018-09-07 15:04:58 +0800

    增加home

--- bookingsystem/urls.py
+++ bookingsystem/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include('home.urls')),
     path('book/',include('book.urls')),
     path('restaurant/',include('restaurant.urls')),
     path('member/',include('member.urls'))
